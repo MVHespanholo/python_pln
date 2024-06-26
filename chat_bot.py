aad3e40ad1fcd16e8defc9c417ffcf0321552163
MVHespanholo <118060937+MVHespanholo@users.noreply.github.com>
2024-06-26 20:40:56 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -25,7 +25,5 @@ if __name__ == "__main__":
     perguntas_respostas = carregar_perguntas("perguntas.txt")
     limiar_distancia = 10
     pergunta = "Quem é Você?"
-        if pergunta == "sair":
-            break
-        resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-        print("Resposta:", resposta)
+    resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
+    print("Resposta:", resposta)
